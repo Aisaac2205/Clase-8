ce15bf1c1095980b1c8a322651f6f7f5470495c0
isaacflores2205@gmail.com <isaacflores2205@gmail.com>
2023-04-01 08:54:55 -0600

    1.0

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿void arreglos ()
+{
+    String[] nombre = new string[]
+    {
+ "Juan", "María", "Pedro"
+    };
+    foreach (String n in nombre)
+    {
+        Console.WriteLine (n);
+    }
+    int[] notas = new int[]
+    {
+    50,30,90
+    };
+    int promedio = 0;
+    foreach (int i in notas)
+    {
+        promedio = promedio + i;
+    }
+    promedio = promedio / notas.Length;
+    Console.WriteLine ($"El promedio es: {promedio}");
+}
+arreglos();
