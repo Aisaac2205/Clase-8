838a8e0c8b00c1ce367c82f6eff6448ed1942f95
Isaac Flores <125110182+Aisaac2205@users.noreply.github.com>
2023-04-14 13:51:16 -0600

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -1,26 +1,56 @@
-﻿void arreglos()
+void arreglos()
 {
-    String[] nombre = new string[]
-{
-    "Fernanda", "Melvin", "Luis", "Brandon", "Yamileth"
-};
+    string[] nombre = new string[]
+    {
+        "Fernanda", "Melvin", "Luis", "Brandon", "Yamileth"
+    };
 
     int[] notas = new int[]
     {
-    70,90,43,85,30
+        70,90,43,85,30
     };
-
     int notaMasAlta = notas[0];
-    int indiceNotaMasAlta = 0;
+    string nombreNotaMasAlta = nombre[0];
 
-    for (int i = 0; i < notas.Length; i++)
+    foreach (int nota in notas)
     {
-        if (notas[i] > notaMasAlta)
+        if (nota > notaMasAlta)
         {
-            notaMasAlta = notas[i];
-            indiceNotaMasAlta = i;
+            notaMasAlta = nota;
         }
     }
-    Console.WriteLine($"La nota más alta es {notaMasAlta} y es del alumno {nombre[indiceNotaMasAlta]}");
-};
+
+    for (int i = 0; i < nombre.Length; i++)
+    {
+        if (notas[i] == notaMasAlta)
+        {
+            nombreNotaMasAlta = nombre[i];
+            break;
+        }
+    }
+    int notaMasBaja = notas[0];
+    string nombreNotaMasBaja = nombre[0];
+
+    foreach (int nota in notas)
+    {
+        if (nota < notaMasBaja)
+        {
+            notaMasBaja = nota;
+        }
+    }
+
+    for (int i = 0; i < nombre.Length; i++)
+    {
+        if (notas[i] == notaMasBaja)
+        {
+            nombreNotaMasBaja = nombre[i];
+            break;
+        }
+    }
+
+    Console.WriteLine($"La nota más alta es {notaMasAlta} y es del alumno {nombreNotaMasAlta}");
+    Console.WriteLine($"La nota más baja es {notaMasBaja} y es del alumno {nombreNotaMasBaja}");
+}
+
 arreglos();
+
